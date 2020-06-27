792ee1b05a291e8f92add70ff9fe8aeab9dd9473
yungxiaomcc <yungxiaomcc@gmail.com>
2020-06-27 18:32:36 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <!-- ------------------------------导航抽屉 -->
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>爬虫</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-cog</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- ---------------------------导航栏 -->
+    <v-app-bar
+      app
+      clipped-right
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>
+          mdi-account-circle
+        </v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>
+          mdi-menu
+        </v-icon> 
+      </v-btn>
+    
+      
+    </v-app-bar>
+    <!-- --------------------------中间内容区 -->
+    <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col class="shrink">
+           
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+    <!------------------------------------ 页脚 -->
+    <v-footer app>
+      <span>&copy; 2020</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    props: {
+      source: String,
+    },
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    data: () => ({
+      drawer: null,
+    }),
+
+    created () {
+      this.$vuetify.theme.dark = true
+    },
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
